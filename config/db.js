a0e7ed80b73625db3c03c5a945f9e7f1d3ff65eb
Enmanuelhdr <ehdelrosario49@gmail.com>
2024-03-24 12:56:53 -0400

    Modificando conexion en de la base de datos

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config({path: '.env'});
 
-const db = new Sequelize(process.env.DB_NOMBRE, process.env.DB_USER, process.env.DB_PASSWORD ?? '', {
+const db = new Sequelize(process.env.DB_NOMBRE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
