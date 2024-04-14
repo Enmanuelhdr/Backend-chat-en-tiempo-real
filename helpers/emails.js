cf847b089c47b8299c751fa589308a2bbcf7b77e
Enmanuelhdr <ehdelrosario49@gmail.com>
2024-04-14 14:44:03 -0400

    Agregando opciones de seguridad al envio de email

--- helpers/emails.js
+++ helpers/emails.js
@@ -9,6 +9,7 @@ const envioDeEmails = async (datos) => {
     const transport = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
