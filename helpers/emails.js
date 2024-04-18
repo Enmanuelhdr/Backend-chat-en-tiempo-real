6ef9a5047a530ce98a6809f354a22ba70c088f76
Enmanuelhdr <ehdelrosario49@gmail.com>
2024-04-18 19:44:43 -0400

    Corrigiendo variables de entorno

--- helpers/emails.js
+++ helpers/emails.js
@@ -7,7 +7,7 @@ dotenv.config({ path: '.env' })
 const envioDeEmails = async (datos) => {
 
     const transport = nodemailer.createTransport({
-        host: smtp.office365.com,
+        host: "smtp.office365.com",
         port: 587,
         secure: false,
         auth: {
